28033981146a844665e2402fb9fae9008fb36b98
RX14 <chris@rx14.co.uk>
2014-11-21 15:26:30 +0000

    Skip coverage on development configuration

--- app/app.rb
+++ app/app.rb
@@ -3,9 +3,11 @@ module ComputingArithmeticQuiz
         register SassInitializer
         register Padrino::Helpers
 
+        # :nocov:
         configure :development do
             use BetterErrors::Middleware
             BetterErrors.application_root = PADRINO_ROOT
         end
+        # :nocov:
     end
 end
