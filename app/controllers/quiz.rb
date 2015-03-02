c34b99de89c17c8c536b15e643e350b0b895a6ad
RX14 <chris@rx14.co.uk>
2015-03-02 19:54:10 +0000

    Add error checking

--- app/controllers/quiz.rb
+++ app/controllers/quiz.rb
@@ -17,12 +17,18 @@ ComputingArithmeticQuiz::App.controllers :quiz do
     get :show, with: :id do
         @quiz = Quiz[params[:id]]
 
+        halt 404, "This Quiz does not exist" if @quiz.nil?
+        halt 410, "This Quiz has already been completed" if @quiz.finished
+
         @title = "Quiz"
         render "quiz/quiz"
     end
 
     get :finish do
         @quiz = Quiz[params[:id]]
+
+        halt 404, "This Quiz does not exist" if @quiz.nil?
+        halt 410, "This Quiz has already been completed" if @quiz.finished
 
         @quiz.answers = Hash[
             @quiz.questions.map do |index, _|
@@ -39,6 +45,9 @@ ComputingArithmeticQuiz::App.controllers :quiz do
 
     get :results, with: :id do
         @quiz = Quiz[params[:id]]
+
+        halt 404, "This Quiz does not exist" if @quiz.nil?
+        halt 400, "This Quiz has not yet been completed" unless @quiz.finished
 
         @title = "Results"
         render "quiz/results"
