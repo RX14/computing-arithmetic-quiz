f45a2398aed73628a6dd13940f5a81a8303210cb
RX14 <chris@rx14.co.uk>
2015-03-18 23:46:50 +0000

    Add class view, with ordering.

--- app/controllers/view.rb
+++ app/controllers/view.rb
@@ -7,4 +7,27 @@ ComputingArithmeticQuiz::App.controllers :view do
         render "view/index"
     end
 
+    get "/class/:class/sort/:sort" do
+        @students = Student.filter(class: params[:class]).all
+
+        case params[:sort]
+            when "alphabetical"
+                @students.sort_by!(&:lastname)
+            when "highest_score"
+                @students.sort_by! { |s| s.highest_score.to_f }
+                @students.reverse!
+            when "average_score"
+                @students.sort_by! { |s| s.average_score.to_f }
+                @students.reverse!
+            else
+                pass
+        end
+
+        @title = "Class: #{params[:class]}"
+        render "view/class"
+    end
+
+    get :student, with: :id do
+
+    end
 end
--- models/student.rb
+++ models/student.rb
@@ -4,4 +4,20 @@ class Student < Sequel::Model
     def name
         firstname + " " + lastname
     end
+
+    def completed_quizzes
+        self.quizzes.select(&:finished)
+    end
+
+    def average_score
+        return if completed_quizzes.empty?
+
+        completed_quizzes.reduce(0.0) { |sum, quiz| sum + quiz.score.to_i } / self.quizzes.size
+    end
+
+    def highest_score
+        return if completed_quizzes.empty?
+
+        completed_quizzes.max_by(&:score).score
+    end
 end
