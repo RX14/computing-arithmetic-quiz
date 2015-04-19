1cd2a316ddb20413b06c8f53d3985f151614df14
RX14 <chris@rx14.co.uk>
2015-04-19 20:36:52 +0100

    Completely comment out specs

--- spec/app/controllers/quiz_controller_spec.rb
+++ spec/app/controllers/quiz_controller_spec.rb
@@ -1,13 +1,13 @@
-require "spec_helper"
-
-RSpec.describe "/quiz" do
-    # pending "add some examples to #{__FILE__}" do
-    #    before do
-    #        get "/test"
-    #    end
-
-    #    it "returns hello world" do
-    #        expect(last_response.body).to eq "Hello World"
-    #    end
-    # end
-end
+# require "spec_helper"
+#
+# RSpec.describe "/quiz" do
+#     pending "add some examples to #{__FILE__}" do
+#        before do
+#            get "/test"
+#        end
+#
+#        it "returns hello world" do
+#            expect(last_response.body).to eq "Hello World"
+#        end
+#     end
+# end
--- spec/app/controllers/view_controller_spec.rb
+++ spec/app/controllers/view_controller_spec.rb
@@ -1,13 +1,13 @@
-require "spec_helper"
-
-RSpec.describe "/view" do
-    pending "add some examples to #{__FILE__}" do
-        before do
-            get "/view"
-        end
-
-        it "returns hello world" do
-            expect(last_response.body).to eq "Hello World"
-        end
-    end
-end
+# require "spec_helper"
+#
+# RSpec.describe "/view" do
+#     pending "add some examples to #{__FILE__}" do
+#         before do
+#             get "/view"
+#         end
+#
+#         it "returns hello world" do
+#             expect(last_response.body).to eq "Hello World"
+#         end
+#     end
+# end
